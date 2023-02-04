807a63a0b8a2c368fbb6d548ba187773044388fa
CodeiWeb <andre.bertolin0@outlook.com>
2023-02-04 11:40:14 -0300

    c

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,17 @@
-function abrirCard {
- 
+function clickAutomotivo() {
+  if (cardAuto.style.display == "block"){
+    cardAuto.style.display = "flex"
+  } else {
+    cardAuto.style.display = "flex"
+    cardEletro.style.display = "none"
   }
+ }
+
+ function clickEletrico() {
+  if (cardEletro.style.display == "flex"){
+    cardEletro.style.display = "flex"
+  } else {
+    cardEletro.style.display = "flex"
+    cardAuto.style.display = "none"
+  }
+ }
